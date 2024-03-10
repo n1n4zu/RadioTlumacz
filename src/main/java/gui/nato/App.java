ca22f719f0d937abc6e01a5fc498a16a3364ea2d
KrI5s <kryspin.dziarekk@gmail.com>
2024-03-10 22:01:26 +0100

    modyfikacja kodu

--- src/main/java/gui/nato/App.java
+++ src/main/java/gui/nato/App.java
@@ -26,4 +26,8 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/gui/nato/Launcher.java
@@ -0,0 +1,8 @@
+package gui.nato;
+
+public class Launcher {
+
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+}
